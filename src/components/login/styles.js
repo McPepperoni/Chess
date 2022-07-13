2e2b55c7870dfce84db2aa7bb2797e190d31cae6
Kaito-Kido <maidailam2001@gmail.com>
2022-07-14 00:11:09 +0700

    Initial Commit

---
+++ src/components/login/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const panel = styled.div`
+    width: 100vw;
+    height: 100vh;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
