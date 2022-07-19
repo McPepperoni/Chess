5523b7cf9796b1d76d6ebb71e01114874bd052ca
McPepperoni <handsomepharaon@gmail.com>
2022-07-19 17:00:26 +0700

    added Castling

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import Board from "../components/board";
 import LegalMoves from "../components/gameLogics/legalMoves";
-import { NumToString, UpdateMap } from "../components/utils/Utils";
+import {
+  NumToString,
+  UpdateEnPassant,
+  UpdateMap,
+} from "../components/utils/Utils";
 
 export default function Game({
   setCurrentPlayer,
@@ -11,6 +15,8 @@ export default function Game({
   setTaken1,
   taken2,
   setTaken2,
+  winner,
+  setWinner,
   ...restProps
 }) {
   const tiles = Array(8).fill(0);
@@ -85,16 +91,25 @@ export default function Game({
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
   ]);
+  const [enPassant, setEnPassant] = useState(new Array(64).fill(false));
 
   const [currentSelect, setCurrentSelect] = useState({ x: -1, y: -1 });
-  const [prevSelect, setPrevSelect] = useState({x: -1, y: -1});
-  const [promote, setPromote] = useState(false)
+  const [prevSelect, setPrevSelect] = useState({ x: -1, y: -1 });
+  const [promote, setPromote] = useState(false);
+  const [swap, setSwap] = useState([]);
 
   return (
     <>
       <Board>
         {children}
-        {promote === true ? <Board.Promote setPromote={setPromote} setMap={setPieces} Map={pieces} pos={prevSelect}></Board.Promote> : null}
+        {promote === true ? (
+          <Board.Promote
+            setPromote={setPromote}
+            setMap={setPieces}
+            Map={pieces}
+            pos={prevSelect}
+          ></Board.Promote>
+        ) : null}
         <Board.Grid>
           {tiles.map((_, i) =>
             tiles.map((_, j) => {
@@ -112,27 +127,74 @@ export default function Game({
                         currentPlayer === pieces[i * 8 + j][1]
                       ) {
                         setCurrentSelect({ x: i, y: j });
-                        setLegalMovesMap(
-                          LegalMoves(
+                        setLegalMovesMap(() => {
+                          var extraMoves = [];
+                          if (pieces[i * 8 + j][0] === 5) {
+                            extraMoves = LegalMoves.King(
+                              { x: i, y: j },
+                              pieces
+                            );
+                          }
+
+                          var moves = LegalMoves(
                             { x: i, y: j },
                             pieces[i * 8 + j][0],
                             pieces
-                          )
-                        );
+                          );
+
+                          var swapAble = [];
+                          extraMoves.forEach((item) => {
+                            moves[item[0]] = 2;
+                            swapAble.push(item);
+                          });
+
+                          setSwap(swapAble);
+
+                          return moves;
+                        });
                       }
                       if (legalMovesMap[i * 8 + j] > 1) {
-                        const update = UpdateMap(pieces, currentSelect, { x: i, y: j }, -1,taken1, taken2)
-                        setPieces(
-                          update[0]
+                        var update = UpdateMap(
+                          pieces,
+                          currentSelect,
+                          { x: i, y: j },
+                          -1,
+                          taken1,
+                          taken2
                         );
-                        setTaken1(update[1])
-                        setTaken2(update[2])
+
+                        var swapIdex = [];
+                        for (var s = 0; s < swap.length; i++) {
+                          if (swap[s][0] === i * 8 + j) {
+                            swapIdex = swap[s][1];
+                            break;
+                          }
+                        }
+
+                        if (swapIdex.length > 0) {
+                          update[0][swapIdex[0]][0] = swapIdex[3];
+                          update[0][swapIdex[1]][0] = swapIdex[2];
+                        }
+
+                        setEnPassant(UpdateEnPassant(pieces, enPassant));
+                        setPieces(update[0]);
+                        setTaken1(update[1]);
+                        setTaken2(update[2]);
                         setPrevSelect({ x: i, y: j });
                         setCurrentSelect({ x: -1, y: -1 });
                         setLegalMovesMap(() => new Array(64).fill(0));
                         setCurrentPlayer(() => (currentPlayer === 1 ? 2 : 1));
 
-                        if(pieces[i * 8 + j][0] === 0 && (i === 0 || i === 7)) {
+                        update[0].find((e) => e[0] === 5 && e[1] === 1)
+                          ? update[0].find((e) => e[0] === 5 && e[1] === 2)
+                            ? setWinner(0)
+                            : setWinner(1)
+                          : setWinner(2);
+
+                        if (
+                          pieces[i * 8 + j][0] === 0 &&
+                          (i === 0 || i === 7)
+                        ) {
                           setPromote(true);
                         }
                       }
